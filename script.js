c1f3b7b0d9e991659ddd7335727910f4b6f6549f
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-10-24 00:00:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function finalizar(){
     if (lista.length == 0){alert('[ERRO] É necessário adicionar ao menos um número!')
     
     }else {
+        let ordem = lista.sort()
+        let primeiro = lista.at(0)
 
         let tam = Number(lista.length)
         let itens = document.createElement('p')
@@ -45,16 +47,17 @@ function finalizar(){
         itens.textContent = `Você inseriu ${tam} ${plural}!`
         res.appendChild(itens)
 
+
         let maior = document.createElement('p')
-        let ordem = lista.sort()
         let ultimo = lista.at(tam - 1)
         maior.textContent = `O maior valor inserido foi ${ultimo}`
         res.appendChild(maior)
 
+
         let menor = document.createElement('p')
-        let primeiro = lista.at(0)
         menor.textContent = `O menor valor inserido foi ${primeiro}`
         res.appendChild(menor)
+
 
         let somatorio = document.createElement('p')
         let soma = 0
@@ -64,11 +67,13 @@ function finalizar(){
         somatorio.textContent = `O somatório de todos os itens é ${soma}`
         res.appendChild(somatorio)
 
+
         let media = document.createElement('p')
         let calc = soma/tam
         media.textContent = `A média dos valores inseridos é ${calc}`
         res.appendChild(media)
 
+        
         let thanks = document.createElement('p')
         thanks.textContent = `Obrigado por utilizar! \u{1F33B}`
         res.appendChild(thanks)
